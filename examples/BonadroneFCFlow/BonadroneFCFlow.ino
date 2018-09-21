28832229dc79a0f267185807a024850c5a76478e
simondlevy <simon.d.levy@gmail.com>
2018-09-20 22:00:15 -0400

    Added example for BonadroneFC

---
+++ examples/BonadroneFCFlow/BonadroneFCFlow.ino
@@ -0,0 +1,28 @@
+#include "PMW3901.h"
+
+// Using digital pin 10 for chip select
+PMW3901 flow(A3, &SPI1);
+
+void setup() {
+  Serial.begin(9600);
+
+  if (!flow.begin()) {
+    Serial.println("Initialization of the flow sensor failed");
+    while(1) { }
+  }
+}
+
+int16_t deltaX,deltaY;
+
+void loop() {
+  // Get motion count since last call
+  flow.readMotionCount(&deltaX, &deltaY);
+
+  Serial.print("X: ");
+  Serial.print(deltaX);
+  Serial.print(", Y: ");
+  Serial.print(deltaY);
+  Serial.print("\n");
+
+  delay(100);
+}
